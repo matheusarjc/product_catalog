ff5ca54d8fe6d22416d7c17f60fe358e79e24f0a
Matheus Araujo <carvalhom630@gmail.com>
2025-06-02 15:26:33 -0300

    chore: add dependencies and first rebase

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,4 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
@@ -18,7 +12,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Kalnia:wght@100..700&display=swap",
   },
 ];
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
     extend: {
       fontFamily: {
         sans: [
-          "Inter",
+          "Kalnia",
           "ui-sans-serif",
           "system-ui",
           "sans-serif",
